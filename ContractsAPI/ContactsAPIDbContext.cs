f35bbfdc986e57211c15e9f0dc52d9fcf6f007bd
HarithHP <hphadaragama@gmail.com>
2023-07-30 22:38:52 +0530

    Add project files.

---
+++ ContractsAPI/ContactsAPIDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ContractsAPI.Models;
+
+namespace ContractsAPI
+{
+    public class ContactsAPIDbContext : DbContext
+    {
+        public ContactsAPIDbContext(DbContextOptions options): base(options) 
+        {
+            
+        }
+        public DbSet<Contact> Contacts { get; set;}
+    }
+}
